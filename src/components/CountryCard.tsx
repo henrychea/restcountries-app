76ba86311bc48c68fe0e252bbf68334862c7d45e
Henry Chea <work@henrychea.com>
2023-05-24 22:49:38 +0700

    Complete list info with sorting

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,31 +1,78 @@
-import { ICountry } from "../types";
+import { ICountry } from '../types'
 
 function CountryCard({ country }: { country: ICountry }) {
-  const { name, cca2, cca3, altSpellings, idd, flag, flags } = country;
-  const common = name?.common;
-  const native = name?.nativeName;
+  const { name, cca2, cca3, altSpellings, idd, flag, flags } = country
+  const common = name?.common
+  const native = name?.nativeName
+
+  const nativeNameKeys = Object.keys(native)
+
+  const nativeKeyedValueObjects = nativeNameKeys.map((key) => {
+    return {
+      keyText: key.toString(),
+      common: native[key].common,
+      official: native[key].official,
+    }
+  })
+  function NativeNameSpan(nativeNameKeyedValue: {
+    keyText: string
+    common: string
+    official: string
+  }) {
+    return (
+      <div
+        className='tooltip'
+        data-tip={`Offical: ${nativeNameKeyedValue.official} ; Common: ${nativeNameKeyedValue.common}`}
+      >
+        <span className='font-bold dark:bg-orange-800 bg-orange-300 text-gray-50 px-2 py-1 rounded dark:text-white mx-1'>
+          {nativeNameKeyedValue.keyText.toUpperCase()}
+        </span>
+      </div>
+    )
+  }
 
   return (
-    <div key={cca2} className="card card-side bg-base-100 shadow-xl w-full min-w-full">
-      <figure className="min-w-[250px]">
+    <div
+      key={cca2 + cca3 + common}
+      className='card card-side bg-base-100 shadow-xl w-full min-w-full'
+    >
+      <figure className='min-w-[250px]'>
         <img
-          className="pl-2 object-contain w-[250px] h-[150px]"
+          className='pl-2 object-contain w-[250px] h-[150px]'
           src={flags?.png}
           alt={flags?.alt}
         />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{`${common} ${flag}`}</h2>
-        <p>Native name : {native?.toString()}</p>
-        <p>Alt spellings : {altSpellings}</p>
-        <p>Country code : {cca2 ? `${cca2} (${cca3})` : 'No CCA'} </p>
+      <div className='card-body'>
+        <h2 className='card-title text-3xl'>{`${common} ${flag}`}</h2>
+        <div>
+          Native name :{' '}
+          {nativeKeyedValueObjects.map((nativeNameKeyedValue, index) => {
+            return (
+              <span key={index}>
+                <NativeNameSpan {...nativeNameKeyedValue} />
+              </span>
+            )
+          })}
+        </div>
+        <p className='space-y-1'>
+          Alternate spellings:{' '}
+          {altSpellings.map((spelling, index) => {
+            return (
+              <span key={index} className='italic flex-grow-0 inline-flex mx-1 mt-1'>
+                {spelling.toString().concat(index !== altSpellings.length - 1 ? ', ' : '') }
+              </span>
+            )
+          })}
+        </p>
+        <p>Country code : <span className='font-bold'>{cca2 ? `${cca2} (${cca3})` : 'No CCA'}</span> </p>
         <p>IID : {idd?.root?.toString()}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">View Details</button>
+        <div className='card-actions justify-end'>
+          <button className='btn btn-primary'>View Details</button>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default CountryCard;
+export default CountryCard
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ICountry } from '../types'
 import CountryCard from './CountryCard'
 
@@ -7,22 +7,32 @@ export default function CountryList(props: { countries: [] }) {
   console.log('Countries', props.countries)
   const [page, setPage] = useState(1)
   const [itemsPerPage, setItemsPerPage] = useState(10)
+  const [sortMode, setSortMode] = useState('')
+  const [displayCountries, setDisplayCountries] = useState([])
   const maxPage = Math.ceil(numberOfCountries / itemsPerPage) - 1
   const pageNumberButtons = []
   const startIndex = (page - 1) * itemsPerPage
   const endIndex = page * itemsPerPage
 
-  const displayCountries = props.countries.slice(
-    startIndex,
-    endIndex
-  )
+  useEffect(() => {
+    setDisplayCountries(props.countries.slice(startIndex, endIndex))
+  }, [page, itemsPerPage, sortMode])
 
   for (let i = 1; i <= maxPage; i++) {
     if (page === i) {
-      pageNumberButtons.push(<button key={i} data-value={i} className='btn btn-active'>{i}</button>)
+      pageNumberButtons.push(
+        <button key={i} data-value={i} className='btn btn-active'>
+          {i}
+        </button>,
+      )
     } else {
       pageNumberButtons.push(
-        <button key={i} data-value={i} onClick={handlePageClick} className='btn'>
+        <button
+          key={i}
+          data-value={i}
+          onClick={handlePageClick}
+          className='btn'
+        >
           {i}
         </button>,
       )
@@ -34,19 +44,63 @@ export default function CountryList(props: { countries: [] }) {
     setPage(page)
   }
 
+  function handleSortMode(event: React.ChangeEvent<HTMLSelectElement>) {
+    const sortMode = event.target.value
+    setSortMode(sortMode)
+    if (sortMode === 'Country Name (Desc)') {
+      const s = props.countries.sort((a: ICountry, b: ICountry) => {
+        return a.name.common.toString() < b.name.common.toString() ? 1 : -1
+      })
+      setDisplayCountries(s.slice(startIndex, endIndex))
+    } else if (sortMode === 'Country Name (Asc)') {
+      const s = props.countries.sort((a: ICountry, b: ICountry) => {
+        return a.name.common.toString() > b.name.common.toString() ? 1 : -1
+      })
+      setDisplayCountries(s.slice(startIndex, endIndex))
+    }
+  }
+
   return (
     <div className='space-y-4 min-w-full'>
+      <div className='flex justify-end'>
+        <div className='text-right pr-2'>Sort</div>
+        <select
+          className='select select-bordered select-xs w-40 max-w-xs'
+          name='itemsDropDown'
+          id='itemsDropDown'
+          onChange={handleSortMode}
+        >
+          <option value={''}>None</option>
+          <option value={'Country Name (Desc)'}>Country Name (Desc)</option>
+          <option value={'Country Name (Asc)'}>Country Name (Asc)</option>
+        </select>
+        <div className='text-right px-2'>Show</div>
+        <select
+          className='select select-bordered select-xs w-24 max-w-xs'
+          name='itemsDropDown'
+          id='itemsDropDown'
+          onChange={(e) => setItemsPerPage(Number(e.target.value))}
+        >
+          <option value={10}>10 items</option>
+          <option value={15}>15 items</option>
+          <option value={20}>20 items</option>
+          <option value={25}>25 items</option>
+          <option value={30}>30 items</option>
+        </select>
+      </div>
       {displayCountries.map((country: ICountry) => {
         return (
-          <div className='row'>
+          <div className='row' key={country.cca2}>
             <CountryCard country={country} />
           </div>
         )
       })}
-      <div className='btn-group'>
-        {pageNumberButtons.map((button) => {
-          return button
-        })}
+      <div className='flex justify-center'>
+        <div className='btn-group'>
+          {pageNumberButtons.map((button) => {
+            return button
+          })}
+        </div>
       </div>
     </div>
   )
